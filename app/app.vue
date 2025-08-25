fcff23835ab6011dadad6286764b315d6d1fbd14
Aceix <aceixsmartx@gmail.com>
2025-08-25 11:46:07 +0000

    chore: init

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <UiButton variant="destructive">Hello Shadcn-Vue!</UiButton>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+
+  devtools: { enabled: true },
+
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "shadcn-nuxt"],
+
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
